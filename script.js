7b1ca11bbb7d66153d5d9bf277b14f92a66725a1
Natalia-Schwindt <naty8014@gmail.com>
2025-03-24 18:53:33 -0300

    realizando estilo de portfolio diferente

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+    document.getElementById("menu-icon").classList.toggle("fa-bars");
+    document.getElementById("menu-icon").classList.toggle("fa-times");
+  }
